ab3dded14b190ced6638f7473590c48229ca15f3
Khalid <fingertechproject@gmail.com>
2022-07-02 01:09:00 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/Demoapi1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoapi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoapi1Application.class, args);
+	}
+
+}
